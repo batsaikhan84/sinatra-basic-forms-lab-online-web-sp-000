5ad6bb848cc2f07160f750cff730616d856f3219
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-05 02:32:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  get '/' do
+    erb :index
+  end
+  
+  post '/'
+    @puppy = Puppy.new(params["name"], params["breed"], params["age"])
+    
+    erb :display_puppy
+  end
 end
